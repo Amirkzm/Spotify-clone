0f82129cd33fc702abcf1bec331f41e7c62e1c75
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-28 00:26:05 +0100

    Change fetch hook to use redux and work with spotify

--- src/hooks/useLazyFetch.ts
+++ src/hooks/useLazyFetch.ts
@@ -1,30 +1,32 @@
+import { Token } from "@mui/icons-material";
 import { useCallback, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../app/store";
 
-const BASE_URL = import.meta.env.VITE_YOUTUBE_API_BASE_URL;
-const key = import.meta.env.VITE_YOUTUBE_API_KEY;
-const credential = `&key=${key}`;
-
-const DEFAULT_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-  },
-};
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+// const key = import.meta.env.VITE_YOUTUBE_API_KEY;
+// const credential = `&key=${key}`;
 
 const useLazyFetch = (comp?: string) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(false);
   const [result, setResult] = useState<any>(null);
+  const { token } = useSelector((state: RootState) => state.userAuth);
+
+  const DEFAULT_OPTIONS = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
   const sendRequest = useCallback(
     async (url: string) => {
       setIsLoading(true);
 
       try {
-        const rawResponse = await fetch(
-          BASE_URL + url + credential,
-          DEFAULT_OPTIONS
-        );
+        const rawResponse = await fetch(BASE_URL + url, DEFAULT_OPTIONS);
         if (!rawResponse.ok) {
           setIsError(true);
           throw new Error("Request failed!");
