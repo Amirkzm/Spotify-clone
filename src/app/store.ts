884fb003b80dee4c25aae6cf3defdbb7f1bed23e
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-27 23:17:50 +0100

    connect userAuthSlice to our store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { userAuthSlice } from "../feature";
 
-export const store = configureStore({
-  reducer: {},
+const store = configureStore({
+  reducer: {
+    userAuth: userAuthSlice,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type appDispatch = typeof store.dispatch;
+export default store;
