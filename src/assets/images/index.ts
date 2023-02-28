544a1d4ed66967b97fc31830aaeb92e7f562d744
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-28 12:13:41 +0100

    Create index file

---
+++ src/assets/images/index.ts
@@ -0,0 +1,3 @@
+export { default as loginBg } from "./login-bg.png";
+export { default as logos } from "./logo.svg";
+export { default as Logo } from "./logo(1).png";
---
+++ src/redux/feature/index.ts
@@ -0,0 +1,1 @@
+export { default as userAuthSlice } from "./userAuth";
