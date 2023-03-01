46d834d15b2b8409e20e2c1cdbb8a5b54395b0ff
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-01 23:07:39 +0100

    set sidebar position to fix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   List,
   ListItemButton,
   ListItemIcon,
@@ -16,58 +17,61 @@ import {
 
 const Sidebar = () => {
   return (
-    <List
-      sx={{
-        width: "100%",
-        maxWidth: 360,
-        bgcolor: "primary.dark",
-        minHeight: "100%",
-      }}
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      subheader={
-        <ListSubheader
-          component="div"
-          id="nested-list-subheader"
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            mb: 20,
-            pt: 5,
-            bgcolor: "primary.dark",
-          }}
-        >
-          <img src={Logo} alt="logo" style={{ borderRadius: "50%" }} />
-        </ListSubheader>
-      }
-    >
-      <Stack sx={{ "&>*:hover *": { color: "#7a9cd2" }, gap: 3 }}>
-        <ListItemButton>
-          <ListItemIcon>
-            <HomeOutlined />
-          </ListItemIcon>
-          <ListItemText primary="Discover" />
-        </ListItemButton>
-        <ListItemButton>
-          <ListItemIcon>
-            <SettingsOverscanOutlined />
-          </ListItemIcon>
-          <ListItemText primary="Around You" />
-        </ListItemButton>
-        <ListItemButton>
-          <ListItemIcon>
-            <StarBorderPurple500Outlined />
-          </ListItemIcon>
-          <ListItemText primary="Top Artists" />
-        </ListItemButton>
-        <ListItemButton>
-          <ListItemIcon>
-            <TagOutlined />
-          </ListItemIcon>
-          <ListItemText primary="Top Charts" />
-        </ListItemButton>
-      </Stack>
-    </List>
+    <Box>
+      <List
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "primary.dark",
+          minHeight: "100vh",
+          // position: "fixed",
+        }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader
+            component="div"
+            id="nested-list-subheader"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              mb: 20,
+              pt: 5,
+              bgcolor: "primary.dark",
+            }}
+          >
+            <img src={Logo} alt="logo" style={{ borderRadius: "50%" }} />
+          </ListSubheader>
+        }
+      >
+        <Stack sx={{ "&>*:hover *": { color: "#7a9cd2" }, gap: 3 }}>
+          <ListItemButton>
+            <ListItemIcon>
+              <HomeOutlined />
+            </ListItemIcon>
+            <ListItemText primary="Discover" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <SettingsOverscanOutlined />
+            </ListItemIcon>
+            <ListItemText primary="Around You" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <StarBorderPurple500Outlined />
+            </ListItemIcon>
+            <ListItemText primary="Top Artists" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <TagOutlined />
+            </ListItemIcon>
+            <ListItemText primary="Top Charts" />
+          </ListItemButton>
+        </Stack>
+      </List>
+    </Box>
   );
 };
 
