dddcfaddc606127970f11eedafc6dc56c100492b
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-01 23:05:11 +0100

    use storage token. add recently played and recommendation endpoint

--- src/redux/services/spotifyCore.ts
+++ src/redux/services/spotifyCore.ts
@@ -26,6 +26,8 @@ interface SpotifySearchResult {
   // ... search result properties
 }
 
+type SeedType = "seed_artists" | "seed_genres" | "seed_tracks";
+
 export const spotifyApi = createApi({
   reducerPath: "spotifyApi",
   baseQuery: fetchBaseQuery({
@@ -34,17 +36,18 @@ export const spotifyApi = createApi({
       console.log(getState());
       const { accessToken } = (getState() as { userAuth: UserAuthState })
         .userAuth;
-
-      if (accessToken) {
-        headers.set("Authorization", `Bearer ${accessToken}`);
+      const storageToken = localStorage.getItem("accessToken");
+      if (accessToken || storageToken) {
+        headers.set("Authorization", `Bearer ${storageToken ?? accessToken}`);
       }
 
       return headers;
     },
   }),
   endpoints: (builder) => ({
-    getTopTracks: builder.query<SpotifyTrack[], void>({
-      query: () => "me/top/tracks",
+    getTopTracks: builder.query<any, { limit?: number }>({
+      query: ({ limit = 12 }) =>
+        `me/top/tracks?time_range=medium_term&limit=${limit}`,
     }),
     getTopArtists: builder.query<SpotifyArtist[], void>({
       query: () => "me/top/artists",
@@ -54,6 +57,16 @@ export const spotifyApi = createApi({
     }),
     getPlaylistTracks: builder.query<SpotifyTrack[], string>({
       query: (playlistId) => `playlists/${playlistId}/tracks`,
+    }),
+    getRecentlyPlayedTracks: builder.query<any, void>({
+      query: () => "me/player/recently-played",
+    }),
+    getRecommendedTracks: builder.query<
+      any,
+      { seedType?: SeedType; seed?: string }
+    >({
+      query: ({ seedType = "seed_genres", seed = "pop,rock" }) =>
+        `https://api.spotify.com/v1/recommendations?limit=10&${seedType}=${seed}`,
     }),
     search: builder.query<SpotifySearchResult, string>({
       query: (searchTerm) =>
@@ -68,4 +81,6 @@ export const {
   useGetPlaylistsQuery,
   useGetPlaylistTracksQuery,
   useSearchQuery,
+  useGetRecentlyPlayedTracksQuery,
+  useGetRecommendedTracksQuery,
 } = spotifyApi;
