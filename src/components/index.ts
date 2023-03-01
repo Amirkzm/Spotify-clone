0a98011986d81c4b003c96d69fc5d84e7e5b718e
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-01 23:05:32 +0100

    Update index to contain new files

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,7 @@ export { default as Sidebar } from "./Sidebar";
 export { default as Searchbar } from "./Searchbar";
 export { default as SongCard } from "./SongCard";
 export { default as SongsFeed } from "./SongsFeed";
-export { default as TopArtists } from "./TopArtists";
+export { default as RecentlyPlayed } from "./RecentlyPlayed";
+export { default as TopCharts } from "./TopCharts";
+export { default as Layout } from "./Layout";
+export { default as UserTopSongs } from "./UserTopSongs";
--- src/redux/services/index.ts
+++ src/redux/services/index.ts
@@ -3,3 +3,5 @@ export { useGetTopArtistsQuery } from "./spotifyCore";
 export { useGetPlaylistsQuery } from "./spotifyCore";
 export { useGetPlaylistTracksQuery } from "./spotifyCore";
 export { useSearchQuery } from "./spotifyCore";
+export { useGetRecentlyPlayedTracksQuery } from "./spotifyCore";
+export { useGetRecommendedTracksQuery } from "./spotifyCore";
