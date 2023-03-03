511b726282707755fdc2bc52853dba9f433c45a5
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-03 23:03:08 +0100

    Change fontSize of headers

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -50,21 +50,21 @@ theme.typography.h1 = {
   fontSize: "clamp(19px,5vw,21px)",
   fontWeight: "700",
   color: theme.palette.text.primary,
-  [theme.breakpoints.up("md")]: { fontSize: "clamp(30px,5vw,34px)" },
+  [theme.breakpoints.up("md")]: { fontSize: "clamp(26px,5vw,30px)" },
 };
 
 theme.typography.h2 = {
   ...theme.typography,
   fontSize: "clamp(17px,5vw,19px)",
   fontWeight: "600",
-  [theme.breakpoints.up("md")]: { fontSize: "clamp(26px,5vw,30px)" },
+  [theme.breakpoints.up("md")]: { fontSize: "clamp(21px,5vw,25px)" },
 };
 
 theme.typography.h3 = {
   ...theme.typography,
   fontSize: "clamp(16px,5vw,18px)",
   fontWeight: "400",
-  [theme.breakpoints.up("md")]: { fontSize: "clamp(23px,5vw,26px)" },
+  [theme.breakpoints.up("md")]: { fontSize: "clamp(19px,5vw,23px)" },
 };
 
 export default theme;
