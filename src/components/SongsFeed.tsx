2c061fbe1968742f3f9fd58509763476b55f0961
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-06 21:36:48 +0100

    use Redux and navigate to page under clicks

--- src/components/SongsFeed.tsx
+++ src/components/SongsFeed.tsx
@@ -2,6 +2,9 @@ import { Stack } from "@mui/material";
 import React from "react";
 import SongCard from "./SongCard";
 import { getAllArtists } from "../utils";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addItme } from "../redux/feature/itemSlice";
 
 interface SongsFeedProps {
   songs: any;
@@ -13,6 +16,11 @@ interface SongType {
 }
 
 const SongsFeed = ({ songs, category = "track" }: SongsFeedProps) => {
+  const dispatch = useDispatch();
+
+  const trackClickHandler = (track: any) => {
+    dispatch(addItme(track));
+  };
   return (
     <Stack
       sx={{
@@ -24,16 +32,20 @@ const SongsFeed = ({ songs, category = "track" }: SongsFeedProps) => {
       }}
       id="root songs feed"
     >
-      {songs.map((song: any, index: number) => {
-        return (
+      {songs.map((song: any, index: number) => (
+        <Link
+          to={`track/${song?.id}`}
+          key={song?.id}
+          onClick={(song) => trackClickHandler}
+        >
           <SongCard
             name={song?.name}
             artist={getAllArtists(song?.artists)}
             imageUrl={song?.album?.images[1]?.url}
             key={index}
           />
-        );
-      })}
+        </Link>
+      ))}
     </Stack>
   );
 };
