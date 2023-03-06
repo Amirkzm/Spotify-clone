7d6409b2e4b571d7843131a2c1a49142f03a8273
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-06 21:29:45 +0100

    Add formatDuration and extractItemProperteis functions

--- src/utils/utilFunctions.ts
+++ src/utils/utilFunctions.ts
@@ -1,5 +1,89 @@
-export const getAllArtists = (data: any) => {
-  const artistList = data.map((artist: any) => artist?.name);
+export const getAllArtists = (data: any): string => {
+  const artistList: any[] = data.map((artist: any) => artist?.name);
   const artistNames: string = artistList.join();
   return artistNames;
+};
+
+export function formatDuration(durationMs: number): string {
+  const totalSeconds = Math.round(durationMs / 1000);
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+}
+
+interface ExtractItemPropertiesProps {
+  item: any;
+  itemType: "playlist" | "track" | "album";
+}
+
+export const extractItemProperties = ({
+  item,
+  itemType,
+}: ExtractItemPropertiesProps) => {
+  let imageUrl,
+    playlistId,
+    artistId,
+    albumId,
+    trackID,
+    songName,
+    albumName,
+    trackDuration,
+    playlistName,
+    artistsName,
+    releaseDate,
+    trakcsPopularity,
+    AlbumTracks,
+    playlistTracks;
+
+  console.log(item);
+
+  switch (itemType) {
+    case "album":
+      imageUrl = item?.images[1]?.url;
+      albumName = item?.name;
+      albumId = item?.id;
+      artistsName = getAllArtists(item?.artists);
+      artistId = item?.artists[0]?.id;
+      releaseDate = item?.release_date;
+      AlbumTracks = item?.tracks?.items;
+      break;
+    case "track":
+      songName = item?.name;
+      imageUrl = item?.album?.images[1]?.url;
+      trackID = item?.id;
+      albumId = item?.album?.id;
+      albumName = item?.album?.name;
+      artistId = item?.artists[0]?.id;
+      artistsName = getAllArtists(item?.artists);
+      trackDuration = item?.duration_ms;
+      trakcsPopularity = item?.popularity;
+      releaseDate = item?.release_date;
+      break;
+    case "playlist":
+      playlistName = item?.name;
+      playlistId = item?.id;
+      imageUrl = item?.images[1]?.url;
+      playlistTracks = item?.tracks?.items;
+      break;
+    default:
+      console.log("GG wp");
+      break;
+  }
+
+  return {
+    imageUrl,
+    playlistId,
+    artistId,
+    albumId,
+    trackID,
+    songName,
+    albumName,
+    trackDuration,
+    playlistName,
+    artistsName,
+    releaseDate,
+    trakcsPopularity,
+    AlbumTracks,
+    playlistTracks,
+  };
 };
