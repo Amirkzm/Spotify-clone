afd62e1d19c83649c50cae4bc6519e98eb70cd96
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-06 21:35:50 +0100

    Custom hook to handle color-thief

---
+++ src/hooks/useImageColor.ts
@@ -0,0 +1,31 @@
+import { useColor } from "color-thief-react";
+import React from "react";
+import { theme } from "../utils";
+
+const useImageColor = (imageUrl: string): string | undefined => {
+  const {
+    data: heroBackgroundColor,
+    loading: imageIsLoading,
+    error: imageIsError,
+  } = useColor(imageUrl, "hex", { crossOrigin: "anonymous" });
+  console.log(imageUrl);
+
+  if (imageIsLoading) {
+    console.log("image is loading");
+  }
+  if (imageIsError) {
+    console.log("an error with image happened");
+  }
+  if (heroBackgroundColor) {
+    console.log("backgeround color = ", heroBackgroundColor);
+  }
+
+  const heroBackground =
+    imageIsLoading || imageIsError
+      ? theme.palette.primary.main
+      : heroBackgroundColor;
+
+  return heroBackground;
+};
+
+export default useImageColor;
