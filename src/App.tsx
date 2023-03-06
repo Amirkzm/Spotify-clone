dcdc58274146a3d339330f114f7f1969a8e83e99
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-06 21:40:09 +0100

    add new paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Discover, Home, Login, Search } from "./pages";
+import { Discover, Home, Library, Login, Search, TrackDetails } from "./pages";
+import AlbumDetails from "./pages/AlbumDetails";
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/discover" element={<Discover />} />
           <Route path="/search/" element={<Search />} />
+          <Route path="/library/" element={<Library />} />
+          <Route path="search/album/:albumId" element={<AlbumDetails />} />
+          <Route path="/album/:trackId" element={<TrackDetails />} />
+          <Route path="/album/:playlistId" element={<AlbumDetails />} />
         </Routes>
       </Box>
     </BrowserRouter>
