0870d77488d81605bbe535e907d67de68bd50453
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-06 21:34:54 +0100

    complete library page

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,7 +1,49 @@
+import { Stack, Typography } from "@mui/material";
 import React from "react";
+import { AlbumFeed, Layout, SongsFeed } from "../components";
+import { useGetSavedAlbumsQuery, useGetSavedTracksQuery } from "../redux";
 
 const Library = () => {
-  return <div>Library</div>;
+  const {
+    data: albumData,
+    isLoading: albumLoading,
+    isError: albumError,
+  } = useGetSavedAlbumsQuery({});
+  const {
+    data: tracksData,
+    isLoading: tracksLoading,
+    isError: tracksError,
+  } = useGetSavedTracksQuery({});
+  if (albumError || tracksError) {
+    return <p>error happened</p>;
+  }
+
+  if (albumLoading || tracksLoading) {
+    return <p>loading items</p>;
+  }
+
+  const albums = albumData?.items.map((item: any) => item?.album);
+  const tracks = tracksData?.items.map((item: any) => item?.track);
+  console.log(albums);
+  console.log(tracks);
+  return (
+    <Layout>
+      <Stack gap={3}>
+        <Stack>
+          <Typography variant="h1" sx={{ position: "relative", top: 50 }}>
+            Saved Albums
+          </Typography>
+          <AlbumFeed albums={albums} />
+        </Stack>
+        <Stack>
+          <Typography variant="h1" sx={{ position: "relative", top: 50 }}>
+            Saved Tracks
+          </Typography>
+          <SongsFeed songs={tracks} />
+        </Stack>
+      </Stack>
+    </Layout>
+  );
 };
 
 export default Library;
