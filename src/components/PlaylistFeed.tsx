ab0bea307554cf41873dfe318bb2ba8971425886
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-06 21:38:38 +0100

    navigate to item page and add item to redux item store

--- src/components/PlaylistFeed.tsx
+++ src/components/PlaylistFeed.tsx
@@ -1,12 +1,18 @@
 import { Stack } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { addItme } from "../redux/feature/itemSlice";
 import SongCard from "./SongCard";
 
 interface PlaylistFeedProps {
   playlists: any;
   category?: string;
 }
-
 const AlbumFeed = ({ playlists, category = "track" }: PlaylistFeedProps) => {
+  const dispatch = useDispatch();
+  const playlistClickHandler = (playlist: any) => {
+    dispatch(addItme(playlist));
+  };
   return (
     <Stack
       sx={{
@@ -18,16 +24,20 @@ const AlbumFeed = ({ playlists, category = "track" }: PlaylistFeedProps) => {
       }}
       id="root playlist feed"
     >
-      {playlists.map((playlist: any, index: number) => {
-        return (
+      {playlists.map((playlist: any, index: number) => (
+        <Link
+          to={"sdalk"}
+          key={playlist?.id}
+          onClick={(playlist) => playlistClickHandler}
+        >
           <SongCard
             name={playlist?.name}
             artist={playlist?.owner?.display_name}
             imageUrl={playlist?.images[1]?.url}
             key={playlist?.id}
           />
-        );
-      })}
+        </Link>
+      ))}
     </Stack>
   );
 };
