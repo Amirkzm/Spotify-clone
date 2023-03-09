e3c01382fd0f765ff981a43c750dbf3807512a82
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-09 23:46:41 +0100

    add some style and html

--- src/pages/AlbumDetails.tsx
+++ src/pages/AlbumDetails.tsx
@@ -1,6 +1,5 @@
-import { Box, CardMedia, Stack, Typography } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
-import { ShowMore } from "../components";
+import { Box, CardMedia, List, Stack, Typography } from "@mui/material";
+import { Layout, ShowMore } from "../components";
 import PopularTrackItem from "../components/PopularTrackItem";
 import { extractItemProperties, formatDuration, theme } from "../utils";
 
@@ -9,6 +8,7 @@ import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { useGetAlbumQuery, useGetAlbumTracksQuery } from "../redux";
+import PlayButton from "../components/PlayButton";
 
 const AlbumDetails = () => {
   const { albumId } = useParams();
@@ -18,10 +18,7 @@ const AlbumDetails = () => {
     isLoading,
     isError,
   } = useGetAlbumTracksQuery(albumId);
-  //   console.log(album.tracks);
-  if (albumTracks) {
-    console.log(albumTracks?.items);
-  }
+
   let { imageUrl, albumName, releaseDate, artistsName } = extractItemProperties(
     {
       item: album,
@@ -33,16 +30,17 @@ const AlbumDetails = () => {
   const releaseYear: string = releaseDate.split("-")[0];
 
   return (
-    <Stack sx={{ minWidth: "100vh" }}>
-      <Box
-        component="section"
-        sx={{
-          display: "flex",
-          background: `${heroBackground}`,
-          minWidth: "500px",
-        }}
-      >
-        <Box>
+    <Layout showRightSidebar>
+      <Stack sx={{ flex: "1 1 auto" }} id="topstackkos">
+        <Box
+          component="section"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            background: `${heroBackground}`,
+            p: 2,
+          }}
+        >
           <CardMedia
             component="img"
             height="240"
@@ -50,39 +48,62 @@ const AlbumDetails = () => {
             alt="album photo"
             sx={{
               objectFit: "contain",
-              mt: 3,
+              m: 3,
               width: "240px",
-              boxShadow: "0px 4px 8px rgba(0,0,0,0.8)",
+              boxShadow: "0px 4px 50px rgba(0,0,0,0.9)",
             }}
           />
+          <Stack>
+            <Typography>Album</Typography>
+            <Typography
+              variant="h1"
+              sx={{ fontSize: "clamp(48px,5vw+1rem,58px)" }}
+            >
+              {albumName}
+            </Typography>
+            <Typography variant="body2">
+              {artistsName}
+              {"."}
+              {releaseYear}
+            </Typography>
+          </Stack>
         </Box>
-        <Box>
-          <Typography>Album</Typography>
-          <Typography variant="h1" sx={{ fontSize: "clamp(48px,5vw,58px)" }}>
-            {albumName}
-          </Typography>
-          <Typography variant="caption">
-            {artistsName}
-            {"."}
-            {releaseYear}
-          </Typography>
-        </Box>
-      </Box>
-      <Stack
-        sx={{
-          background: `linear-gradient(to left, #134c88,${heroBackground} )`,
-        }}
-      >
-        <Stack direction={"row"}></Stack>
-        <ShowMore minHeight={500}>
-          <Box component="section">
-            {albumTracks?.items.map((item: any) => (
-              <PopularTrackItem key={item?.id} trackItem={item} />
-            ))}
+        <Stack
+          sx={{
+            background: `linear-gradient(to bottom, ${heroBackground},#134c88 )`,
+            width: "100%",
+            flex: "1 1 auto",
+            pt: 10,
+          }}
+          id="bottom part container"
+        >
+          <Box sx={{ pl: 3, mb: 3, mt: -10 }}>
+            <PlayButton sx={{ fontSize: "45px" }} />
           </Box>
-        </ShowMore>
+          <Typography variant="h3" sx={{ pl: 4 }}>
+            Title
+          </Typography>
+          <Box
+            sx={{
+              width: "95%",
+              height: "1px",
+              bgcolor: "rgba(255,255,255,0.2)",
+              alignSelf: "center",
+              ml: 3,
+            }}
+          ></Box>
+
+          <ShowMore minHeight={380}>
+            <List component="ol" sx={{ minWidth: "100%" }} id="kirehendi">
+              {albumTracks?.items.map((item: any) => (
+                <PopularTrackItem key={item?.id} trackItem={item} />
+              ))}
+            </List>
+          </ShowMore>
+        </Stack>
+        <Box></Box>
       </Stack>
-    </Stack>
+    </Layout>
   );
 };
 
