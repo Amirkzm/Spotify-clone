d53215480e89d245fbe25c4eb9b0af4f0d9d9c2c
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-09 23:51:21 +0100

    create component

---
+++ src/components/ExplicitIcon.tsx
@@ -0,0 +1,11 @@
+import { Card, Tooltip } from "@mui/material";
+
+const ExplicitIcon = () => {
+  return (
+    <Tooltip title="Explicit">
+      <Card sx={{ p: 0.5 }}>E</Card>
+    </Tooltip>
+  );
+};
+
+export default ExplicitIcon;
