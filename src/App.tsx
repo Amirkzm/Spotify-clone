3008cf4a5baf51b5053112b6e8ba655b89a6d237
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-09 23:52:19 +0100

    add new paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { Box } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Discover, Home, Library, Login, Search, TrackDetails } from "./pages";
+import {
+  ArtistDetails,
+  Discover,
+  Home,
+  Library,
+  Login,
+  PlaylistDetails,
+  Search,
+  TrackDetails,
+  UserLibrary,
+} from "./pages";
 import AlbumDetails from "./pages/AlbumDetails";
 
 function App() {
+  console.log("running app");
   return (
     <BrowserRouter>
       <Box sx={{ backgroundColor: "common.black" }}>
@@ -12,10 +23,12 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/discover" element={<Discover />} />
           <Route path="/search/" element={<Search />} />
-          <Route path="/library/" element={<Library />} />
-          <Route path="search/album/:albumId" element={<AlbumDetails />} />
-          <Route path="/album/:trackId" element={<TrackDetails />} />
-          <Route path="/album/:playlistId" element={<AlbumDetails />} />
+          {/* <Route path="/library/" element={<Library />} /> */}
+          <Route path="/album/:albumId" element={<AlbumDetails />} />
+          <Route path="/track/:trackId" element={<TrackDetails />} />
+          <Route path="/playlist/:playlistId" element={<PlaylistDetails />} />
+          <Route path="/artist/:artistId" element={<ArtistDetails />} />
+          <Route path="/library" element={<UserLibrary />} />
         </Routes>
       </Box>
     </BrowserRouter>
