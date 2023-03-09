1d92400acc3489e5c7ffdf9892a420fd50b769ec
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-09 23:50:09 +0100

    add some style

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -11,9 +11,14 @@ const PlayButton = ({ sx }: PlayButtonProps) => {
   const [play, setPlay] = useState<boolean>(true);
   return (
     <Box>
-      <IconButton onClick={() => setPlay((prev) => !prev)}>
+      <Box
+        onClick={() => setPlay((prev) => !prev)}
+        sx={{
+          "&:hover": { cursor: "pointer " },
+        }}
+      >
         {play ? <PlayCircleIcon sx={sx} /> : <PauseCircleIcon sx={sx} />}
-      </IconButton>
+      </Box>
     </Box>
   );
 };
