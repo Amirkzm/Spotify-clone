32923ab0e12bacc78207cf6420d423b352b092c2
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-09 23:49:20 +0100

    rewrite popularTrackItem

--- src/components/PopularTrackItem.tsx
+++ src/components/PopularTrackItem.tsx
@@ -1,30 +1,61 @@
-import { CardMedia, List, ListItemButton, Typography } from "@mui/material";
-import { formatDuration } from "../utils";
+import {
+  Box,
+  Divider,
+  ListItem,
+  ListItemIcon,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { formatDuration, getAllArtists } from "../utils";
+import ExplicitIcon from "./ExplicitIcon";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
 interface TrackDetailsProps {
   trackItem: any;
 }
 
 const PopularTrackItem = ({ trackItem }: TrackDetailsProps) => {
+  const [show, setShow] = useState<boolean>(false);
   return (
-    <List>
-      <ListItemButton sx={{ display: "flex", justifyContent: "space-between" }}>
-        <CardMedia
-          component="img"
-          height="60"
-          image={trackItem?.album?.images[1]?.url}
-          alt="album photo"
-          sx={{
-            objectFit: "contain",
-            width: "60px",
-          }}
-        />
-        <Typography variant="body2">{trackItem?.popularity}</Typography>
-        <Typography variant="body2">
-          {formatDuration(trackItem?.duration_ms)}
-        </Typography>
-      </ListItemButton>
-    </List>
+    <ListItem
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        position: "relative",
+        "&:hover": { bgcolor: "hsla(0,0%,100%,.2)" },
+      }}
+      onMouseEnter={() => setShow(true)}
+      onMouseLeave={() => setShow(false)}
+    >
+      <Stack direction={"row"}>
+        {show && (
+          <ListItemIcon sx={{ position: "absolute", top: "25%", left: "0" }}>
+            <PlayArrowIcon
+              sx={{
+                fontSize: "35px",
+                "&:hover": { fill: "#f74336", cursor: "pointer" },
+              }}
+            />
+          </ListItemIcon>
+        )}
+        <Box sx={{ ml: 3 }}>
+          <Link to={`/track/${trackItem?.id}`}>
+            <Typography>{trackItem?.name}</Typography>
+          </Link>
+          <Box sx={{ display: "flex", alignItems: "end", gap: 1 }}>
+            {trackItem?.explicit && <ExplicitIcon />}
+            <Typography component={"span"}>
+              {getAllArtists(trackItem?.artists)}
+            </Typography>
+          </Box>
+        </Box>
+      </Stack>
+      <Typography variant="body1">
+        {formatDuration(trackItem?.duration_ms)}
+      </Typography>
+    </ListItem>
   );
 };
 
