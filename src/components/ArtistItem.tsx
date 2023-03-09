860962cfa258920cf52c143b8c19fa61985c3bd2
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-09 23:51:45 +0100

    wrap cardMedia inside a link component

--- src/components/ArtistItem.tsx
+++ src/components/ArtistItem.tsx
@@ -1,21 +1,34 @@
 import { CardMedia } from "@mui/material";
+import { Link } from "react-router-dom";
 
 interface ArtistItemProps {
   imageUrl: string;
+  size?: number;
+  artistId: string;
 }
 
 const ArtistItem = (props: ArtistItemProps) => {
-  const { imageUrl } = props;
+  const { imageUrl, size, artistId } = props;
+
+  const handleClick = () => {
+    console.log("item clicked");
+  };
 
   return (
-    <CardMedia
-      component="img"
-      height="100px"
-      image={imageUrl}
-      alt="album photo"
-      sx={{ objectFit: "cover", mt: 3, width: "100px", borderRadius: "50%" }}
-      id="kireasbeabi"
-    />
+    <Link to={`/artist/${artistId}`} onClick={() => handleClick()}>
+      <CardMedia
+        component="img"
+        height={size ?? "100px"}
+        image={imageUrl}
+        alt="album photo"
+        sx={{
+          objectFit: "cover",
+          mt: 3,
+          width: size ?? "100px",
+          borderRadius: "50%",
+        }}
+      />
+    </Link>
   );
 };
 
