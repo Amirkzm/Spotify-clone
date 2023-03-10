7cd19e0ffe6a32d0a8f2038399caf726a9a9de8e
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-10 23:52:27 +0100

    use tracksList component and remove background image cover

--- src/pages/ArtistDetails.tsx
+++ src/pages/ArtistDetails.tsx
@@ -5,6 +5,7 @@ import { Layout, ShowMore } from "../components";
 import ArtistItem from "../components/ArtistItem";
 import PlayButton from "../components/PlayButton";
 import PopularTrackItem from "../components/PopularTrackItem";
+import TracksList from "../components/TracksList";
 import useImageColor from "../hooks/useImageColor";
 import {
   useGetArtistQuery,
@@ -50,50 +51,53 @@ const ArtistDetails = () => {
 
   return (
     <Layout showRightSidebar>
-      <Stack sx={{ width: "100%" }} id="root stack artist">
-        <Box
+      <Stack sx={{ width: "100%", minHeight: "100vh" }} id="root stack artist">
+        <Stack
+          direction={"row"}
           sx={{
-            backgroundImage: `url(${imageUrl})`,
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center center",
-            backgroundSize: "100% 100%",
-            backgroundAttachment: "fixed",
-            minHeight: 640,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            gap: 2,
-            p: 4,
+            background: `linear-gradient(to right,${backgroundColor},#243f68)`,
           }}
         >
-          <Typography
-            variant="body1"
+          <Box sx={{ alignSelf: "center" }}>
+            <ArtistItem imageUrl={imageUrl} size={400} shouldNavigate={false} />
+          </Box>
+          <Box
             sx={{
-              "&": { fontSize: "clamp(58px,5vw,68px)" },
-              textStroke: "1px black",
-              WebkitTextStroke: "1px black",
+              minHeight: 400,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-around",
+              gap: 2,
+              p: 4,
             }}
           >
-            Artist
-          </Typography>
-          <Typography
-            variant="h1"
-            sx={{
-              "&": { fontSize: "clamp(58px,5vw,68px)" },
-              textStroke: "1px black",
-              WebkitTextStroke: "1px black",
-            }}
-          >
-            {artistsName}
-          </Typography>
-          <Typography
-            variant="h2"
-            fontWeight={"bold"}
-            sx={{ textStroke: "1px black", WebkitTextStroke: "1px black" }}
-          >
-            {followers} follow {artistsName}
-          </Typography>
-        </Box>
+            <Box>
+              <Typography
+                variant="body1"
+                sx={{
+                  "&": { fontSize: "clamp(38px,5vw,48px)" },
+                }}
+              >
+                Artist
+              </Typography>
+              <Typography
+                variant="h1"
+                sx={{
+                  "&": { fontSize: "clamp(68px,5vw,78px)" },
+                }}
+              >
+                {artistsName}
+              </Typography>
+            </Box>
+            <Typography
+              variant="h2"
+              fontWeight={"bold"}
+              sx={{ textStroke: "1px black", WebkitTextStroke: "1px black" }}
+            >
+              {followers} follow {artistsName}
+            </Typography>
+          </Box>
+        </Stack>
         <Stack
           sx={{
             background: `linear-gradient(to bottom, ${backgroundColor},#134c88 )`,
@@ -108,10 +112,10 @@ const ArtistDetails = () => {
           >
             <Stack direction={"row"}>
               <PlayButton sx={{ fontSize: "68px" }} />
-              <Typography variant="body1" sx={{ alignSelf: "center" }}>
-                {artistGenres.join(", ")}
-              </Typography>
             </Stack>
+            <Typography variant="body1" sx={{ alignSelf: "center" }}>
+              {artistGenres.join(", ")}
+            </Typography>
             <Typography variant="body1" sx={{ alignSelf: "center" }}>
               0-100 popularity:{artistPopularity}
             </Typography>
@@ -119,33 +123,7 @@ const ArtistDetails = () => {
           <Typography variant="h2" sx={{ pl: 4 }}>
             Top Tracks by {artistsName}
           </Typography>
-          <Stack direction={"row"} justifyContent="space-around">
-            <Typography variant="h3" sx={{ pl: 4 }}>
-              Title
-            </Typography>
-            <Typography variant="h3" sx={{ position: "relative", left: "25%" }}>
-              Release Date
-            </Typography>
-            <Typography variant="h3" sx={{ pr: 1 }}>
-              Duration
-            </Typography>
-          </Stack>
-          <Box
-            sx={{
-              width: "95%",
-              height: "1px",
-              bgcolor: "rgba(255,255,255,0.2)",
-              alignSelf: "center",
-              ml: 3,
-            }}
-          ></Box>
-          <ShowMore minHeight={500}>
-            <Box component="section">
-              {topTracks?.tracks.map((item: any) => (
-                <PopularTrackItem key={item?.id} trackItem={item} />
-              ))}
-            </Box>
-          </ShowMore>
+          <TracksList tracks={topTracks?.tracks} height={500} />
           <Typography variant="h2">You may also like</Typography>
           <Stack
             direction={"row"}
@@ -168,102 +146,3 @@ const ArtistDetails = () => {
 };
 
 export default ArtistDetails;
-
-/*<Layout showRightSidebar>
-      <Stack sx={{ width: "100%" }}>
-        <Box
-          sx={{
-            backgroundImage: `url(${imageUrl})`,
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center center",
-            backgroundSize: "100% 100%",
-            backgroundAttachment: "fixed",
-            minHeight: 640,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            gap: 2,
-            p: 4,
-          }}
-        >
-          <Typography
-            variant="body1"
-            sx={{
-              "&": { fontSize: "clamp(58px,5vw,68px)" },
-              textStroke: "1px black",
-              WebkitTextStroke: "1px black",
-            }}
-          >
-            Artist
-          </Typography>
-          <Typography
-            variant="h1"
-            sx={{
-              "&": { fontSize: "clamp(58px,5vw,68px)" },
-              textStroke: "1px black",
-              WebkitTextStroke: "1px black",
-            }}
-          >
-            {artistsName}
-          </Typography>
-          <Typography
-            variant="h2"
-            fontWeight={"bold"}
-            sx={{ textStroke: "1px black", WebkitTextStroke: "1px black" }}
-          >
-            {followers} follow {artistsName}
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            background: `linear-gradient(to bottom,${backgroundColor},#243f68)`,
-            width: "100%",
-            flex: "1 1 auto",
-          }}
-          id="kosehendiekhar"
-        >
-          <Box>
-            <Box>
-              <PlayButton />
-              <Typography variant="body1">{artistGenres.join(", ")}</Typography>
-            </Box>
-            <Typography variant="body1">
-              0-100 popularity:{artistPopularity}
-            </Typography>
-          </Box>
-          <Typography variant="h3" sx={{ pl: 4 }}>
-            Title
-          </Typography>
-          <Box
-            sx={{
-              width: "95%",
-              height: "1px",
-              bgcolor: "rgba(255,255,255,0.2)",
-              alignSelf: "center",
-              ml: 3,
-            }}
-          ></Box>
-          <Box sx={{ width: "95%" }}>
-            <ShowMore minHeight={480}>
-              {topTracks?.tracks.map((track: any) => (
-                <PopularTrackItem trackItem={track} key={track?.id} />
-              ))}
-            </ShowMore>
-          </Box>
-          <Typography variant="h2">You may also like</Typography>
-          <Stack
-            direction={"row"}
-            gap={1}
-            sx={{ overflow: "scroll", width: "100%" }}
-          >
-            {recommendedArtists?.artists.map((artist: any) => (
-              <ArtistItem
-                imageUrl={artist?.images[1]?.url}
-                artistId={artist?.id}
-                size={200}
-                key={artist?.id}
-              />
-            ))}
-          </Stack>
-        </Box>
-      </Stack>*/
