d161698ca67af94dbe4e2f850a9e10fb37423839
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-10 23:56:13 +0100

    add shouldNavigate to handle if component should have link or not

--- src/components/ArtistItem.tsx
+++ src/components/ArtistItem.tsx
@@ -1,34 +1,52 @@
-import { CardMedia } from "@mui/material";
+import { Box, CardMedia } from "@mui/material";
 import { Link } from "react-router-dom";
 
 interface ArtistItemProps {
   imageUrl: string;
   size?: number;
-  artistId: string;
+  artistId?: string;
+  shouldNavigate?: boolean;
 }
 
 const ArtistItem = (props: ArtistItemProps) => {
-  const { imageUrl, size, artistId } = props;
+  const { imageUrl, size, artistId, shouldNavigate = true } = props;
 
   const handleClick = () => {
     console.log("item clicked");
   };
 
   return (
-    <Link to={`/artist/${artistId}`} onClick={() => handleClick()}>
-      <CardMedia
-        component="img"
-        height={size ?? "100px"}
-        image={imageUrl}
-        alt="album photo"
-        sx={{
-          objectFit: "cover",
-          mt: 3,
-          width: size ?? "100px",
-          borderRadius: "50%",
-        }}
-      />
-    </Link>
+    <Box>
+      {shouldNavigate ? (
+        <Link to={`/artist/${artistId}`} onClick={() => handleClick()}>
+          <CardMedia
+            component="img"
+            height={size ?? "100px"}
+            image={imageUrl}
+            alt="album photo"
+            sx={{
+              objectFit: "cover",
+              mt: 3,
+              width: size ?? "100px",
+              borderRadius: "50%",
+            }}
+          />
+        </Link>
+      ) : (
+        <CardMedia
+          component="img"
+          height={size ?? "100px"}
+          image={imageUrl}
+          alt="album photo"
+          sx={{
+            objectFit: "cover",
+            mt: 3,
+            width: size ?? "100px",
+            borderRadius: "50%",
+          }}
+        />
+      )}
+    </Box>
   );
 };
 
