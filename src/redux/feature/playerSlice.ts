5adf7ffd29db1e2f98c87e83821a01cb24ed6ab5
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-12 23:32:29 +0100

    add trackQueue and showplayer

--- src/redux/feature/playerSlice.ts
+++ src/redux/feature/playerSlice.ts
@@ -2,16 +2,20 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface PlayerSlice {
   track: any;
+  trackQueue: any[];
   isPlaying: boolean;
   nextTrack: any;
   previousTrack: any;
+  showPlayer?: boolean;
 }
 
 const initialState: PlayerSlice = {
   track: null,
+  trackQueue: [],
   isPlaying: false,
   nextTrack: null,
   previousTrack: null,
+  showPlayer: false,
 };
 
 const playerSlice = createSlice({
@@ -23,9 +27,20 @@ const playerSlice = createSlice({
       state.isPlaying = action.payload.isPlaying;
       state.nextTrack = action.payload.nextTrack;
       state.previousTrack = action.payload.previousTrack;
+      state.trackQueue = action.payload.trackQueue;
+      state.showPlayer = true;
+    },
+    updateTrack: (
+      state: PlayerSlice,
+      action: PayloadAction<PlayerSlice["track"]>
+    ) => {
+      state.track = action.payload.track;
+    },
+    hidePlayer: (state: PlayerSlice) => {
+      state.showPlayer = false;
     },
   },
 });
 
-export const { addTrack } = playerSlice.actions;
+export const { addTrack, updateTrack, hidePlayer } = playerSlice.actions;
 export default playerSlice.reducer;
