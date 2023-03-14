ac61961471090d43b415b8dc4897d713b1e55357
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-14 23:49:59 +0100

    use showPlayer to set pb

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,10 +1,15 @@
 import { Box, Stack } from "@mui/material";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 import NewReleasesTrack from "./NewReleases";
 import TopArtists from "./TopArtists";
 
 const RightSidebar = () => {
+  const showPlayer = useSelector(
+    (state: RootState) => state.itemToPlay.showPlayer
+  );
   return (
-    <Box>
+    <Box sx={{ pb: showPlayer ? "205px" : "0" }}>
       <Stack sx={{ overflow: "hidden", height: "100vh" }} id="rightSidebarRoot">
         <NewReleasesTrack />
         <Box sx={{ width: "100%", overflow: "scroll" }}>
