f2ff72ccf708ff903eb53e34bfc496c817982039
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-14 23:45:18 +0100

    Remove logs

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,6 +14,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 
 const Login = () => {
+  console.log("running login");
   const { oauthHandler } = useLogin();
   const [open, setOpen] = useState<boolean>(true);
   const { accessToken, accessGranted } = useSelector(
