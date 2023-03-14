9ee37f7ec8fab4e0a6a79f552abe9cd0345735c0
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-14 23:50:57 +0100

    remove return in isloading and iserror states

--- src/components/RecentlyPlayed.tsx
+++ src/components/RecentlyPlayed.tsx
@@ -16,23 +16,28 @@ const RecentlyPlayed = () => {
     // console.log(listOfSongs);
   }
 
-  if (isLoading) {
-    return <p>loading recently played songs</p>;
-  }
+  // if (isLoading) {
+  //   return <p>loading recently played songs</p>;
+  // }
 
-  if (isError) {
-    return <p>error happend while loading recently played songs</p>;
-  }
-  const listOfSongs = recentSongs?.items.map((item: any) => item?.track);
-  const modifiedList = removeDuplicates(listOfSongs);
+  // if (isError) {
+  //   return <p>error happend while loading recently played songs</p>;
+  // }
+  const listOfSongs =
+    recentSongs && recentSongs?.items.map((item: any) => item?.track);
+  const modifiedList = recentSongs && removeDuplicates(listOfSongs);
 
   return (
-    <Stack>
-      <Typography variant="h2" sx={{ alignSelf: "start", mt: 5 }}>
-        Recently Played songs
-      </Typography>
-      <SongsFeed songs={modifiedList} />
-    </Stack>
+    <>
+      {!isLoading && !isError && (
+        <Stack>
+          <Typography variant="h2" sx={{ alignSelf: "start", mt: 5 }}>
+            Recently Played songs
+          </Typography>
+          <SongsFeed songs={modifiedList} />
+        </Stack>
+      )}
+    </>
   );
 };
 
