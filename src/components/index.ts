c2e9b4a94749408ad288572d0e28c85539b177b9
Amir Kazemi <kazemi.amir68@gmail.com>
2023-03-16 20:58:11 +0100

    update index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Sidebar } from "./Sidebar";
-export { default as Searchbar } from "./Searchbar";
 export { default as SongCard } from "./SongCard";
 export { default as SongsFeed } from "./SongsFeed";
 export { default as RecentlyPlayed } from "./RecentlyPlayed";
