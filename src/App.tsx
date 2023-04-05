b1fa035f49802341f6394f2b6f54126204721e57
Amir Kazemi <kazemi.amir68@gmail.com>
2023-04-05 14:41:36 +0200

    now protect routes from unauthorised users

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { Box } from "@mui/material";
+import { Alert, Box } from "@mui/material";
+import { Fragment, useState } from "react";
 import { useSelector } from "react-redux";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Router } from "react-router-dom";
 import Header from "./components/Header";
 import Player from "./components/player/Player";
+import ProtectedRoute from "./components/ProtectedRoute";
 import {
   ArtistDetails,
   Discover,
   Home,
-  Library,
   Login,
   PlaylistDetails,
   Search,
@@ -15,29 +16,38 @@ import {
   UserLibrary,
 } from "./pages";
 import AlbumDetails from "./pages/AlbumDetails";
+import NotFound from "./pages/NotFound";
 import { RootState } from "./redux/store";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
 
 function App() {
-  console.log("running app");
+  const isUserAuthenticated = useSelector(
+    (state: RootState) => state.userAuth.accessGranted
+  );
   const showPlayer = useSelector(
     (state: RootState) => state.itemToPlay.showPlayer
   );
+
   return (
     <BrowserRouter>
       <Box sx={{ backgroundColor: "common.black" }}>
         <Header />
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/discover" element={<Discover />} />
-          <Route path="/search/" element={<Search />} />
-          <Route path="/album/:albumId" element={<AlbumDetails />} />
-          <Route path="/track/:trackId" element={<TrackDetails />} />
-          <Route path="/playlist/:playlistId" element={<PlaylistDetails />} />
-          <Route path="/artist/:artistId" element={<ArtistDetails />} />
-          <Route path="/library" element={<UserLibrary />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/discover" element={<Discover />} />
+            <Route path="/search/" element={<Search />} />
+            <Route path="/album/:albumId" element={<AlbumDetails />} />
+            <Route path="/track/:trackId" element={<TrackDetails />} />
+            <Route path="/playlist/:playlistId" element={<PlaylistDetails />} />
+            <Route path="/artist/:artistId" element={<ArtistDetails />} />
+            <Route path="/library" element={<UserLibrary />} />
+          </Route>
+          {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
-        <Player />
+
+        {isUserAuthenticated && showPlayer && <Player />}
       </Box>
     </BrowserRouter>
   );
