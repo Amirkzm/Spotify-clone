2a1dbe20d1c9102f08fc9025d7d9ef9bae825572
Amir Kazemi <kazemi.amir68@gmail.com>
2023-04-05 14:26:25 +0200

    redesign login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,11 @@
 import {
   Alert,
   Button,
-  Container,
   IconButton,
   Snackbar,
   Typography,
+  Stack,
+  Box,
 } from "@mui/material";
 import { Close } from "@mui/icons-material";
 import loginBackground from "../assets/images/login-bg.png";
@@ -39,57 +40,101 @@ const Login = () => {
   );
 
   return (
-    <Container
-      sx={{
-        minHeight: "100vh",
-        width: "80vw",
-        background: `url(${loginBackground}) no-repeat fixed center`,
-        backgroundSize: { xs: "cover", lg: "contain" },
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <Stack
+      sx={{ background: "linear-gradient(to bottom right, #001029, #134c88)" }}
     >
-      <Button
-        variant="outlined"
+      <Stack
         sx={{
-          minHeight: "100px",
-          minWidth: "400px",
-          display: "flex",
+          flexDirection: "row",
           justifyContent: "center",
           alignItems: "center",
-          bgcolor: "primary.main",
+          gap: 5,
         }}
-        onClick={oauthHandler}
       >
+        <Box sx={{ mt: 2 }}>
+          <img
+            src={loginBackground}
+            height={"700"}
+            style={{ minHeight: "90vh", width: "100%", minWidth: "425px" }}
+          />
+        </Box>
+        <Stack gap={5} justifyContent={"space-between"}>
+          <Typography
+            variant="h1"
+            sx={{ maxWidth: "600px", minWidth: "180px" }}
+          >
+            Listen to song previews, discover new artists, and connect with your
+            favorite tracks like never before.
+          </Typography>
+          <Typography
+            sx={{
+              textDecoration: "none",
+              fontFamily: "Abril_Fatface",
+              fontSize: "15px",
+              fontWeight: "900",
+              color: "white",
+            }}
+          >
+            first, you need to login to your spotify account
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{
+              minHeight: "80px",
+              maxWidth: "100px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              bgcolor: "primary.main",
+            }}
+            onClick={oauthHandler}
+          >
+            <Typography
+              sx={{
+                textDecoration: "none",
+                fontFamily: "Abril_Fatface",
+                fontSize: "15px",
+                fontWeight: "900",
+                color: "white",
+              }}
+            >
+              Login
+            </Typography>
+          </Button>
+        </Stack>
+        {!accessGranted && accessGranted !== null && (
+          <Snackbar
+            open={open}
+            autoHideDuration={6000}
+            onClose={handleClose}
+            message="Access not granted"
+            action={action}
+            sx={{ color: "error" }}
+          >
+            <Alert
+              onClose={handleClose}
+              severity="error"
+              sx={{ width: "100%" }}
+            >
+              Access not granted!
+            </Alert>
+          </Snackbar>
+        )}
+      </Stack>
+      <Box sx={{ width: "100%", height: "1px", bgcolor: "white" }}></Box>
+      <Stack sx={{ justifyContent: "center", alignItems: "center" }}>
         <Typography
+          variant="body1"
           sx={{
-            textDecoration: "none",
-            fontFamily: "Abril_Fatface",
-            fontSize: "15px",
-            fontWeight: "900",
-            color: "white",
+            fontFamily: "cursive",
+            fontSize: "25px !important",
+            fontWeight: "800 !important",
           }}
         >
-          Login To Your Spotify ACcount
+          Design and Created by Amir Kazemi
         </Typography>
-      </Button>
-      {!accessGranted && accessGranted !== null && (
-        <Snackbar
-          open={open}
-          autoHideDuration={6000}
-          onClose={handleClose}
-          message="Access not granted"
-          action={action}
-          sx={{ color: "error" }}
-        >
-          <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
-            Access not granted!
-          </Alert>
-        </Snackbar>
-      )}
-    </Container>
+      </Stack>
+    </Stack>
   );
 };
 
