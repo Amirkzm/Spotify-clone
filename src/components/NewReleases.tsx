bd70f93e03c0d7dc476a704d3ac3d6fcbf6ea656
Amir Kazemi <kazemi.amir68@gmail.com>
2023-04-05 14:38:35 +0200

    some tiny style changes

--- src/components/NewReleases.tsx
+++ src/components/NewReleases.tsx
@@ -1,5 +1,4 @@
 import { Stack, Typography } from "@mui/material";
-import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { useGetAlbumTracksQuery, useGetNewReleasesQuery } from "../redux";
@@ -12,7 +11,7 @@ import ReleaseItem from "./ReleaseItem";
 const NewReleasesTrack = () => {
   const dispatch = useDispatch();
   const { data, isLoading, isError } = useGetNewReleasesQuery({
-    limit: 5,
+    limit: 4,
   });
 
   if (isLoading) {
@@ -28,7 +27,7 @@ const NewReleasesTrack = () => {
   };
 
   return (
-    <Stack sx={{ pt: 10 }}>
+    <Stack sx={{ pt: 10, gap: 1 }}>
       <Typography variant="h2" sx={{ transform: "translateX(20px)" }}>
         New Releases
       </Typography>
