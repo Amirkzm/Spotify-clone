96c47e45407be6a3d0f2e54dc1a01e96a0f24a03
Amir Kazemi <kazemi.amir68@gmail.com>
2023-04-05 14:41:10 +0200

    now only consist of a cardmedia and no-link rrd component

--- src/components/ArtistItem.tsx
+++ src/components/ArtistItem.tsx
@@ -1,51 +1,29 @@
 import { Box, CardMedia } from "@mui/material";
-import { Link } from "react-router-dom";
 
 interface ArtistItemProps {
   imageUrl: string;
   size?: number;
-  artistId?: string;
-  shouldNavigate?: boolean;
+  // artistId?: string;
+  // shouldNavigate?: boolean;
 }
 
 const ArtistItem = (props: ArtistItemProps) => {
-  const { imageUrl, size, artistId, shouldNavigate = true } = props;
-
-  const handleClick = () => {
-    console.log("item clicked");
-  };
+  const { imageUrl, size } = props;
 
   return (
     <Box>
-      {shouldNavigate ? (
-        <Link to={`/artist/${artistId}`} onClick={() => handleClick()}>
-          <CardMedia
-            component="img"
-            height={size ?? "100px"}
-            image={imageUrl}
-            alt="album photo"
-            sx={{
-              objectFit: "cover",
-              mt: 3,
-              width: size ?? "100px",
-              borderRadius: "50%",
-            }}
-          />
-        </Link>
-      ) : (
-        <CardMedia
-          component="img"
-          height={size ?? "100px"}
-          image={imageUrl}
-          alt="album photo"
-          sx={{
-            objectFit: "cover",
-            mt: 3,
-            width: size ?? "100px",
-            borderRadius: "50%",
-          }}
-        />
-      )}
+      <CardMedia
+        component="img"
+        height={size ?? "100px"}
+        image={imageUrl}
+        alt="album photo"
+        sx={{
+          objectFit: "cover",
+          mt: 3,
+          width: size ?? "100px",
+          borderRadius: "50%",
+        }}
+      />
     </Box>
   );
 };
