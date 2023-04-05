2542856c8b945369d39220749d32436f07f5b2c8
Amir Kazemi <kazemi.amir68@gmail.com>
2023-04-05 14:36:15 +0200

    add framer-motion

--- src/components/SongsFeed.tsx
+++ src/components/SongsFeed.tsx
@@ -1,4 +1,5 @@
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import { motion } from "framer-motion";
 import SongCard from "./SongCard";
 import { getAllArtists } from "../utils";
 import { Link } from "react-router-dom";
@@ -15,7 +16,6 @@ interface SongType {
 }
 
 const SongsFeed = ({ songs }: SongsFeedProps) => {
-  console.log("song feed is running");
   const dispatch = useDispatch();
 
   const trackClickHandler = (track: any) => {
@@ -28,16 +28,21 @@ const SongsFeed = ({ songs }: SongsFeedProps) => {
       })
     );
   };
+
+  const cardVariants = {
+    hidden: { opacity: 0, x: -5, y: -5 },
+    visible: { opacity: 1, x: 0, y: 0 },
+  };
+
   return (
     <Stack
       sx={{
         flexDirection: "row",
         gap: 2,
         flexWrap: "wrap",
-        justifyContent: "start",
-        pt: 10,
+        justifyContent: "center",
+        pt: 5,
       }}
-      id="root songs feed"
     >
       {songs.map((song: any, index: number) => (
         <Link
@@ -45,12 +50,27 @@ const SongsFeed = ({ songs }: SongsFeedProps) => {
           key={song?.id}
           onClick={() => trackClickHandler(song)}
         >
-          <SongCard
-            name={song?.name}
-            artist={getAllArtists(song?.artists)}
-            imageUrl={song?.album?.images[1]?.url}
-            key={index}
-          />
+          <Box
+            component={motion.div}
+            initial="hidden"
+            animate="visible"
+            variants={cardVariants}
+            sx={{
+              "&:hover": {
+                transform: "scale(1.05) !important",
+              },
+            }}
+            transition={{ duration: 0.3, delay: index * 0.1 }}
+            whileTap={{ scale: 0.95 }}
+            whileInView={cardVariants.visible}
+          >
+            <SongCard
+              name={song?.name}
+              artist={getAllArtists(song?.artists)}
+              imageUrl={song?.album?.images[1]?.url}
+              key={index}
+            />
+          </Box>
         </Link>
       ))}
     </Stack>
